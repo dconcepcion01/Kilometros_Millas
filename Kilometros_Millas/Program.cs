8b0e050cf4618ac678a95317c4ca9cb7cbe50b0b
Danny <dannyconcepcion01@gmail.com>
2020-09-04 20:07:42 -0400

    Agregar archivos de proyecto.

---
+++ Kilometros_Millas/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kilometros_Millas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //este programa convierte de kilometros a milla
+
+            double millas, kilometros, resultados;
+
+            Console.WriteLine("Digite la cantidad de millas");
+            millas = double.Parse(Console.ReadLine());
+
+            kilometros = millas  / 0.62137;
+            kilometros = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("La cantidad de millas a kilometros es:  " + kilometros);
+            Console.ReadKey();
+
+
+        }
+    }
+}
